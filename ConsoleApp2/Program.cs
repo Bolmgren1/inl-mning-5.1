3c72893a3c99969d571f995d65dceba7b188426c
Acrobaticbag3 <sibe05001@utb.vaxjo.se>
2023-01-20 10:33:23 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många meningar vill du skriva?");
+            int antal = int.Parse(Console.ReadLine());
+            string[] meningar = new string[antal];
+
+            for (int i = 0; i < antal; i++)
+            {
+                Console.WriteLine("Skriv in din mening här");
+                meningar[i] = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("Dessa är de meningarna du skrev in");
+            for (int i = 0; i < antal; i++)
+            {
+                Console.WriteLine(meningar[i]);
+            }
+            
+
+            
+        }
+    }
+}
